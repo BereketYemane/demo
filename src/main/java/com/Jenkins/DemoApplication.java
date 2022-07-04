3209b690652372305fec88e53557bcee3c5bb065
BereketYemane <berie20.yemane@gmail.com>
2022-07-03 22:39:28 -0500

    wiP

--- src/main/java/com/Jenkins/DemoApplication.java
+++ src/main/java/com/Jenkins/DemoApplication.java
@@ -1,25 +1,14 @@
 package com.Jenkins;
 
 
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.annotation.PostConstruct;
-import java.util.logging.Logger;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static Logger logger =  (Logger) LoggerFactory.getLogger(DemoApplication.class);
-
-	@PostConstruct
-	public void starter() {
-		logger.info("Application started");
-	}
 
 	public static void main(String[] args) {
-		logger.info("Application executed");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
